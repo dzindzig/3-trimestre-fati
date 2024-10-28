b56661541a7826ff49d7ad670c057e2ac5dd3995
dzindzig <147923340+dzindzig@users.noreply.github.com>
2024-10-28 09:34:24 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o maior osso do corpo humano?',
+    'Fêmur'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a guerra que parrou o mundo?',
+    'A segunda guerra mundial'
+)
